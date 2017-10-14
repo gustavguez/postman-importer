b1c34e6ef6738f404074e1dda1f2ce0b3be95893
gustavo-rodriguez <1990Cynet@gmail.com>
2017-10-14 14:55:38 -0300

    refactor(index.php): refactor output results style

--- index.php
+++ index.php
@@ -10,4 +10,8 @@ $importer = new Importer(dirname(__FILE__));
 $result = $importer->run();
 
 //Print result
-var_dump($result);
+printf('POSTMAN IMPORTER RESULTS%s', PHP_EOL);
+printf('--------------------------------%s', PHP_EOL);
+foreach ($result as $file => $result) {
+    printf('- SOURCE FILE: %s, STATUS: %s %s', $file, $result ? 'OK' : 'FAIL', PHP_EOL);
+}
